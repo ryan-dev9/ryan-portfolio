a7b64f3211305776a9201d93c78e55f36ec39202
Ryan Developer <ryandeveloper97@gmail.com>
2025-11-11 21:03:22 +0530

    I-S2.5

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-   experimental: {
-    turbo: {
-      // Turbopack production beta enable
-    },
-  },
 };
 
 export default nextConfig;
